a35bebcc6e1b626a119e7a5105c2ae2acc73b475
stotoki <totoki.sayoko@gmail.com>
2024-03-18 22:01:52 +0900

    added authentication

--- js/auth.js
+++ js/auth.js
@@ -4,7 +4,32 @@ import firebaseConfig from "./firebaseApikey.js";
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-import { getDatabase, ref, push, set, onChildAdded }
-    from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged }
+            from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
+const app = initializeApp(firebaseConfig);
+
+// Google Auth(認証用)
+    const provider = new GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+    const auth = getAuth();
+
+// login処理
+    $(".socialBtn1").on("click", function() {
+        // google認証完了後の処理
+        signInWithPopup(auth, provider).then((result) => {
+            // login後のページ遷移
+            location.href = "memo_app.html"; //遷移先
+        }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+
+            // The email of the user's account used.
+            const email = error.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+        })
+    })
